26a6797d2f1531ca0f32281d51c14d7cba12f02f
Zhenning Ruan <rzning@outlook.com>
2017-05-03 23:13:57 +0800

    Updated : demo `jstree`

--- _samples/jstree/main.js
+++ _samples/jstree/main.js
@@ -11,4 +11,46 @@ $(function() {
         $('#jstree').jstree('select_node', 'child_node_1');
         $.jstree.reference('#jstree').select_node('child_node_1');
     });
+
+
+    // 示例二
+    $('#jstree_demo').jstree({
+    "core" : {
+        "animation" : 0,
+        "check_callback" : true,
+        "themes" : { "stripes" : true },
+        'data' : {
+        'url' : function (node) {
+            return node.id === '#' ?
+            'ajax_demo_roots.json' : 'ajax_demo_children.json';
+        },
+        'data' : function (node) {
+            return { 'id' : node.id };
+        }
+        }
+    },
+    "types" : {
+        "#" : {
+        "max_children" : 1,
+        "max_depth" : 4,
+        "valid_children" : ["root"]
+        },
+        "root" : {
+        "icon" : null,
+        "valid_children" : ["default"]
+        },
+        "default" : {
+        "valid_children" : ["default","file"]
+        },
+        "file" : {
+        "icon" : "glyphicon glyphicon-file",
+        "valid_children" : []
+        }
+    },
+    "plugins" : [
+        "contextmenu", "dnd", "search",
+        "state", "types", "wholerow"
+    ]
+    });
 });
+
